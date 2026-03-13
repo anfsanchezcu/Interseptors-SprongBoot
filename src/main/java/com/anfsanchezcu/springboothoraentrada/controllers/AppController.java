00558ae0c357cb67f10c4ff7ed963a613095f0cf
anfsanchezcu <anfsanchezcu@unal.edu.co>
2026-03-12 21:45:04 -0500

    final: intersptor that reconize when a petition is over time and dicide if take or not

--- src/main/java/com/anfsanchezcu/springboothoraentrada/controllers/AppController.java
+++ src/main/java/com/anfsanchezcu/springboothoraentrada/controllers/AppController.java
@@ -7,18 +7,20 @@ import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 public class AppController {
 
   @GetMapping("/foo")  
-  public ResponseEntity<?> foo(){
-
-
+  public ResponseEntity<?> foo(HttpServletRequest request){
     Map<String, Object> data = new HashMap<>();
+
+    data.put("messageByInterseptor",  request.getAttribute("message"));
     data.put("message", "Hello World");
     data.put("time", new Date());
     return ResponseEntity.ok(data);
--- src/main/java/com/anfsanchezcu/springboothoraentrada/interseptors/CalendarInterseptor.java
+++ src/main/java/com/anfsanchezcu/springboothoraentrada/interseptors/CalendarInterseptor.java
@@ -1,35 +1,53 @@
 package com.anfsanchezcu.springboothoraentrada.interseptors;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.jspecify.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import tools.jackson.databind.ObjectMapper;
 
-@Component
-public class CalendarInterseptor  implements HandlerInterceptor{
+@Component("calendarInterseptor")
+public class CalendarInterseptor implements HandlerInterceptor {
+
+  @Value("${config.calendar.open}")
+  private int openHour;
+  @Value("${config.calendar.close}")
+  private int closeHour;
 
   @Override
-  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) 
-    throws Exception {
-      Calendar calendar = Calendar.getInstance();
-      int hour = calendar.get(Calendar.HOUR_OF_DAY);
-    return true;
-  }
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+      throws Exception {
+    Calendar calendar = Calendar.getInstance();
+    int hour = calendar.get(Calendar.HOUR_OF_DAY);
+    Map<String, Object> res = new HashMap<>();
 
+    if (openHour <= hour && hour < closeHour) {
+
+      request.setAttribute("message", "Gracias por visitarnos, estamos abiertos");
+      return true;
+    }
+
+    ObjectMapper mapper = new ObjectMapper();
+    res.put("message", "Gracias por visitarnos, estamos cerrados");
+    response.setContentType("application/json");
+    response.setStatus(401);
+    response.getWriter().write(mapper.writeValueAsString(res));
+    return false;
+  }
 
   @Override
   public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
       @Nullable ModelAndView modelAndView) throws Exception {
-    
+
     HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
   }
-
-  
-  
 
 }
